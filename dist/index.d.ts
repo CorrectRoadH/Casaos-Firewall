23d944481afa330a1deb9d71753c12bd23230617
CtRdH <a778917369@gmail.com>
2023-05-27 20:44:11 +0800

    release

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { ApiError } from './core/ApiError';
-export { CancelablePromise, CancelError } from './core/CancelablePromise';
-export { OpenAPI } from './core/OpenAPI';
-export type { OpenAPIConfig } from './core/OpenAPI';
-export type { BaseResponse } from './models/BaseResponse';
-export type { Rule } from './models/Rule';
-export { NftableMethodsService } from './services/NftableMethodsService';
--- route/v2/nftables.go
+++ route/v2/nftables.go
@@ -1,1 +1,12 @@
 package v2
+
+import (
+	"net/http"
+
+	codegen "github.com/CorrectRoadH/CasaOS-Firewall/codegen"
+	"github.com/labstack/echo/v4"
+)
+
+func (s *Firewall) GetRules(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, codegen.GetRulesResponseOK{})
+}
