b7f470175f73e14456b47c3a49f5756d51184c9a
CorrectRoadH <a778917369@gmail.com>
2023-05-30 14:59:09 +0800

    refactor store

--- casaos-firewall-frontend/src/stores/firewall.ts
+++ casaos-firewall-frontend/src/stores/firewall.ts
@@ -1,13 +1,40 @@
-import { ref, computed } from 'vue'
+import { ref, computed, reactive } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
+import type { Port } from '@/types'
 
-export const useFirewallStore = defineStore('firewall', () => {
-  const port = ref(0)
-  const firewallState = ref(true)
-  const firewallVersion = ref('')
-  function getPort() {
-    
+const baseHost = 'http://localhost'
+const PortMetaData = {
+  fromJson(object: any):Port{
+    return {
+      port: object.port ? object.port : "error port",
+      protocol: object.protocol ? object.protocol : "error protocol",
+      action: object.action ? object.port : "error action",
+    }
   }
-  
-  return { port, getPort }
+}
+
+interface FirewallState {
+  Ports: Port[],
+  firewallState: boolean,
+  firewallVersion: string,
+}
+
+export const useFirewallStore = defineStore('firewall', {
+  state: (): FirewallState => ({
+    Ports : [],
+    firewallState : true,
+    firewallVersion : "",
+  }),
+  actions: {
+    async getPort() {
+      const ports = (await axios.get(baseHost+'/v2/firewall/port')).data
+      this.Ports = ports.map(PortMetaData.fromJson)
+    },
+    async getFirewallState() {},
+    async getFirewallVersion() {
+      const version = (await axios.get(baseHost+'/v2/firewall/version')).data
+      this.firewallVersion = version
+    }
+  }
 })
