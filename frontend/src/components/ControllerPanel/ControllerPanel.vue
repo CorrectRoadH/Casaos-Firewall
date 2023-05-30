ae493d972fb2c8f8a87551549a8ace5f8566ea9d
CtRdH <a778917369@gmail.com>
2023-05-30 21:39:47 +0800

    extract portable

--- frontend/src/components/ControllerPanel/ControllerPanel.vue
+++ frontend/src/components/ControllerPanel/ControllerPanel.vue
@@ -1,28 +1,10 @@
 <script setup lang="ts">
-import {onMounted, ref } from 'vue';
-import axios from 'axios';
-import CButton from '../kit/CButton.vue';
+import {onMounted } from 'vue';
 import { useFirewallStore } from '../../stores/firewall';
 import PortEdit from './PortEdit.vue';
-
+import PortTable from './PortTable.vue';
 
 const firewallState = useFirewallStore();
-const baseHost = "http://127.0.0.1"
-
-const closePort = async(port:string,protocol:string):Promise<any> => {
-  const promise = axios.post(baseHost+'/v2/firewall/nftables',{
-    port: port,
-    protocol: protocol,
-    action: "close"
-  })
-  return promise
-}
-
-const handelCloseBtnClick = async (port:string,protocol:string)=>{
-  await closePort(port,protocol)
-  firewallState.getPort()
-  alert("success")
-}
 
 onMounted(()=>{
   firewallState.getPort()
@@ -40,35 +22,13 @@ defineExpose({
         <p>These ports that didn't list in below is prohibited by default.</p>
     </div>
     <PortEdit/> 
+    <PortTable />
     <!-- <div v-if="isLoading" >
       Loading...
     </div>
     <div v-if="isError">
       Fetching Error
     </div> -->
-    <div v-if="firewallState.Ports">
-      <table  class="table">
-        <thead>
-          <tr>
-            <th>Port</th>
-            <th>State</th>
-            <th>Protocol</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item) in firewallState.Ports" :key="item.port">
-            <td>{{item.port}}</td>
-            <td>Open</td>
-            <td>{{item.protocol}}</td>
-            <td>
-              <CButton @click="handelCloseBtnClick(item.port,item.protocol)">Close</CButton>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-    </div>
   </main>
 </template>
 <style scoped>
---
+++ frontend/src/components/ControllerPanel/PortTable.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { useFirewallStore } from '../../stores/firewall';
+import CButton from '../kit/CButton.vue';
+
+const firewallState = useFirewallStore();
+
+const handelCloseBtnClick = async (port:string,protocol:string)=>{
+  await firewallState.setPort(port,protocol,"close")
+  firewallState.getPort()
+  alert("success")
+}
+
+</script>
+
+<template>
+    <div class="table" v-if="firewallState.Ports">
+        <table  class="table">
+          <thead>
+            <tr>
+              <th>Port</th>
+              <th>State</th>
+              <th>Protocol</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item) in firewallState.Ports" :key="item.port">
+              <td>{{item.port}}</td>
+              <td>Open</td>
+              <td>{{item.protocol}}</td>
+              <td>
+                <CButton @click="handelCloseBtnClick(item.port,item.protocol)">Close</CButton>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+  
+      </div>
+</template>
+<style scoped>
+.table{
+    width: 100%;
+    border-collapse: collapse;
+    margin: auto;
+    margin-top: 20px;
+    border-radius: 5px;
+    flex-grow: 2;
+}
+</style>
